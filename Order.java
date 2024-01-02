d1eea263d1a6b6f01a184b45e170847adb0289cd
devJWL <jwlee4450@gmail.com>
2024-01-02 20:47:36 +0900

    Order 클래스 완료

--- Order.java
+++ Order.java
@@ -1,9 +1,39 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
 
 public class Order {
-    List<Product> menuList;
+    Map<Product, Integer> menuList;
     public Order() {
-        this.menuList = new ArrayList<>();
+        this.menuList = new HashMap<>();
+    }
+    // 장바구니에 추가하는 메소드
+    public void addOrder(Product product) {
+        menuList.put(product, menuList.getOrDefault(product, 0) + 1);
+    }
+
+
+    // 장바구니를 비우는 메소드
+    public void orderCancel() {
+        menuList.clear();
+    }
+
+
+    // 물품의 갯수를 한개씩 빼는 메소드
+    public void subtractOrder(Product product) {
+        int cnt = menuList.getOrDefault(product, -1);
+        --cnt;
+        // 장바구니에 없는 물건이 input으로 들어온 경우
+        if (cnt == -2) return;
+        // 마지막 한 개를 비운 경우
+        if (cnt == 0) {
+            menuList.remove(product);
+        }
+        // 한개 이상 남은 경우
+        else {
+            menuList.put(product, cnt);
+        }
     }
 }
