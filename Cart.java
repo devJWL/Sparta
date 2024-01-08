1e928a27ea479a6c1e40070756d9f05fd5655133
devJWL <jwlee4450@gmail.com>
2024-01-08 10:52:51 +0900

    수정사항 없음

--- Cart.java
+++ Cart.java
@@ -42,21 +42,4 @@ public class Cart {
         linkedSet.clear();
         menuHash.clear();
     }
-
-    // 물품의 갯수를 한개씩 빼는 메소드
-    public void subtract (Product product) {
-        int cnt = menuHash.getOrDefault(product, -1);
-        // 장바구니에 없는 물건이 input으로 들어온 경우
-        if (cnt == -1) return;
-        --cnt;
-        // 마지막 한 개를 비운 경우
-        if (cnt == 0) {
-            linkedSet.remove(product);
-            menuHash.remove(product);
-        }
-        // 한 개 이상 남은 경우
-        else {
-            menuHash.put(product, cnt);
-        }
-    }
 }
